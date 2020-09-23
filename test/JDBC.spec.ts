1eddcbd0b9736920cd3562e9ee030dee03767267
MRW Neundorf <ninevillage+naxmefy@gmail.com>
2020-09-23 23:31:07 +0200

    test: added spec to test a jdbc statement

--- test/JDBC.spec.ts
+++ test/JDBC.spec.ts
@@ -22,4 +22,33 @@ describe('JDBC', () => {
         should.exist(connection)
       })
   })
+
+  it('should create and execute a statement', async () => {
+    const jdbc = new JDBC({
+      className: 'org.h2.Driver',
+      url: 'jdbc:h2:mem:test'
+    })
+
+    const statement = await jdbc.createStatement()
+    await statement.executeUpdate(`
+      CREATE TABLE jdbc_test_table (
+        id int not null
+      )
+    `)
+
+    const result = await statement.executeUpdate(`
+      INSERT INTO jdbc_test_table (id) VALUES
+      (1),
+      (2)
+    `)
+
+    result.should.be.equal(2)
+
+    const result2 = await statement.executeQuery(`
+        SELECT * FROM jdbc_test_table
+    `)
+
+    const resultSet = result2.fetchAllResults()
+    resultSet.should.have.length(2)
+  })
 })
